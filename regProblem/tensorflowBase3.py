31db193c39cebb9bbdd3c728dce68aef195c5b90
yonghang.bai <yonghang.bai@vipshop.com>
2021-06-16 22:57:00 +0800

    update

--- regProblem/tensorflowBase3.py
+++ regProblem/tensorflowBase3.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 import numpy as np
+# 加入忽略
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # 广播
 # 广播机制是一种轻量级的张量复制手段，在逻辑上拓展张量的形状，在需要时才去执行存储复制操作
@@ -10,3 +14,19 @@ b = tf.random.uniform([3], maxval=10)
 # 等效于 y=x @ w + tf.broadcast(b,[2,3])
 y = x @ w + b
 print(y)
+
+# 基本的加减乘除运算已经被tensorFlow重载可直接使用运算符
+a = tf.range(5)
+b = tf.constant(2)
+print(a / b)
+
+# 乘方运算 tf.pow(a,x) a**x
+a = tf.range(4)
+print(a)
+print(tf.pow(a, 3))
+print(a ** 3)
+# 平方
+tf.square(a)
+
+# 指数运算 tf.pow(x,a) x**a
+print(tf.pow(3, a))
